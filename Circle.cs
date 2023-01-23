8a3927c0c92e90c348da14e245cf6aa118d9ec7f
chris <chrisabelw@m3.com>
2023-01-23 17:02:02 -0500

    first commit

---
+++ Circle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Circle_Objects_Lab
+{
+	public class Circle
+	{
+		//properties
+		public double Radius { get; set; }
+
+		private string FormatNumber { get; set; }
+
+		//constructor
+		public Circle(double _radius)
+		{
+			Radius = _radius;
+		}
+
+		//methods
+		public double CalculatedCircumference()
+		{
+			double circleCircumference = 2 * Radius * Math.PI;
+			return circleCircumference;
+        }
+	
+		public string CalculatedFormattedCircumference()
+		{
+			return $"Circumference: {GetFormatNumber(CalculatedCircumference())}";
+		}
+
+		public double CalculatedArea()
+		{
+			double circleArea = Math.PI * Radius * Radius;
+			return circleArea;
+		}
+
+		public string CalculatedFormattedArea()
+		{
+			return $"Area: {GetFormatNumber(CalculatedArea())}";
+		}
+
+		public string GetFormatNumber(double x)
+		{
+			return Math.Round(x, 2).ToString();
+		}
+	
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Circle_Objects_Lab;
+
+Console.WriteLine("Welcome to the Circle Tester!");
+
+bool runProgram = true;
+while (runProgram)
+{
+
+    Console.WriteLine("Please enter a radius. (Allows Decimal): ");
+
+    double userNum = double.Parse(Console.ReadLine());
+
+   
+    if (userNum > 0)
+    {
+        Circle myCircle = new Circle(userNum);
+        Console.WriteLine($"The circumference is {myCircle.CalculatedFormattedCircumference()}");
+        Console.WriteLine("");
+        Console.WriteLine($"The area is {myCircle.CalculatedFormattedArea()}");
+    }
+    else if(userNum < 0)
+    {
+        Console.WriteLine("Invaild input please try again!"); 
+    }
+
+    Console.WriteLine("Would you like to try again? y/n");
+    string choice = Console.ReadLine().ToLower().Trim();
+    if(choice == "y" || choice == "yes")
+    {
+        runProgram = true;
+    }
+    else if(choice == "n" || choice == "no")
+    {
+        runProgram = false;
+        Console.WriteLine("Thanks for playing!");
+    }
+
+}
+
